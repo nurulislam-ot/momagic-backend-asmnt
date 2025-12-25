b80768ab9f8f5a9d358f586d26deebf0d03a7861
nurulislam-ot <laam105722@gmail.com>
2025-12-25 22:55:44 +0600

    refactor: remove server listen logic and export app for deployment feat: add Vercel configuration for edge runtime

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,4 @@ const PORT = process.env.PORT
 app.use("/products", productsRouter)
 app.use("/categories", categoriesRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+export default app
